68769d4f409364f248168259c6f33865d248f8dc
ArtemVolovyk <artem.volovyk@marconirovereto.it>
2026-02-05 11:13:33 +0100

    feat: Implement print, size, attach, and slice methods in DoublyLinkedList

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,3 +1,5 @@
+
+
 public class DoublyLinkedList {
     
     private Node head, tail;
@@ -46,13 +48,115 @@ public class DoublyLinkedList {
     // ------------ INSERT YOUR CODE HERE ----------------
 
     public void print(boolean forward) {
+        if(forward){
+            printForward(head);
+        } else {
+            printBackward(tail);
+        }
+        System.out.println();
+    }
 
+    private void printForward(Node cursor){
+        if (cursor == null) return;
+        System.out.print(cursor.getLetter());
+        printForward(cursor.getNext());
+    }
+
+    private void printBackward(Node cursor){
+        if (cursor == null) return;
+        System.out.print(cursor.getLetter());
+        printBackward(cursor.getNext());
     }
 
     public int size() {
-
-        return 0;
+        int count = 0;
+        Node cursor = head;
+        while (cursor != null){
+            count++;
+            cursor = cursor.getNext();
+        }
+        return count;
     }
+
+
+    // if cuurent empty then becomes newList 
+    public void attach(DoublyLinkedList newList){
+        if ( newList == null || newList.head == null) return;
+
+        if (this.head == null){
+            this.head = newList.head;
+            this.tail = newList.tail;
+            return;
+        }
+
+        newList.tail.setNext(this.head);
+        this.head.setPrev(newList.tail);
+
+        this.head = newList.head;
+    }
+
+    public String slice (int start, int end){
+        int n = size();
+
+        if (start < 0) start = 0;
+        if (start < n) start = n;
+        if (end < 0) end = 0;
+        if (end > n) end = n;
+
+        if ( start== end ) return "";
+
+        StringBuilder sb = new StringBuilder();
+
+        if (start < end){
+            Node cur = get(start);
+            int i = start;
+
+            while ( cur != null && i < end){
+                sb.append(cur.getLetter());
+                cur = cur.getNext();
+                i++;
+            }
+        } else{
+            Node cur = get(start);
+            int i = start;
+
+            while ( cur != null && i > end){
+                sb.append(cur.getLetter());
+                cur = cur.getPrev();
+                i--;
+            }
+        }
+        return sb.toString();
+    }
+
+    public void removeRepetitions(){
+        if ( head == null ) return;
+
+        Node cur = head;
+
+        while ( cur != null && cur.getNext() != null){
+            
+            if(cur.getLetter() == cur.getNext().getLetter()){
+                Node remove = cur.getNext();
+                Node removed = remove.getNext();
+
+                cur.setNext(removed);
+
+                if ( removed != null ){
+                    removed.setPrev(cur);
+                } else {
+                    tail = cur; // remove tail
+                }
+            } else {
+                cur = cur.getNext();
+            }
+        }
+    }
+
+    public void mirror(int index){
+        
+    }
+
 
 
 }
